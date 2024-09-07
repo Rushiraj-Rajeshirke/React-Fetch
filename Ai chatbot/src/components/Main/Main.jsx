c34b900d97dfa256311fea22c588666e8502f872
Rushiraj Rajeshirke <154682776+Rushiraj-Rajeshirke@users.noreply.github.com>
2024-09-07 21:37:43 +0530

    second commit

--- Ai chatbot/src/components/Main/Main.jsx
+++ Ai chatbot/src/components/Main/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
   return (
     <div className='main'>
       <div className='nav'>
-        <p>Gemini 1.0 Pro</p>
+        <p>Powered By Gemini</p>
         <img src={assets.user_icon} alt="user icon" />
       </div>
       <div className="main-container">
